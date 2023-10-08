67f7a636d808b42c4346d0aa558ce765143659e5
Jijuhwan <38065739+Jijuhwan@users.noreply.github.com>
2023-10-08 12:18:00 +0900

    Update 문자열 밀기.java

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,6 +1,16 @@
 class Solution {
     public int solution(String A, String B) {
         int answer = 0;
-        return answer;
+        String chk = A;
+        
+        for(int i=0; i<A.length(); i++){
+            if(chk.equals(B)) return answer;
+            
+            String temp = chk.substring(chk.length()-1);
+            chk = temp + chk.substring(0, chk.length()-1);
+            answer++;
+        }
+        
+        return -1;
     }
 }
