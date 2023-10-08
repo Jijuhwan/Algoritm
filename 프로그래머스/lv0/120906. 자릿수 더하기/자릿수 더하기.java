2add57fc8e33ba085dde7baef7640ecfd92ec526
Jijuhwan <38065739+Jijuhwan@users.noreply.github.com>
2023-10-08 12:22:52 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.15 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,6 +1,10 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        String[] chk = String.valueOf(n).split("");
+        for(String c : chk){
+            answer += Integer.valueOf(c);
+        }
         return answer;
     }
 }
