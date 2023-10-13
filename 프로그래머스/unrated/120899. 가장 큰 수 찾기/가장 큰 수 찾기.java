3794d7424aa24a0b528dc287560849c3cf410404
Jijuhwan <38065739+Jijuhwan@users.noreply.github.com>
2023-10-13 22:37:40 +0900

    Update 가장 큰 수 찾기.java

--- 프로그래머스/unrated/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/unrated/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,6 +1,14 @@
 class Solution {
     public int[] solution(int[] array) {
-        int[] answer = {};
+        int[] answer = {array[0], 0};
+        
+        for(int i=1; i<array.length; i++){
+            if(answer[0] < array[i]){
+                answer[0] = array[i];
+                answer[1] = i;
+            }
+        }
+        
         return answer;
     }
 }
