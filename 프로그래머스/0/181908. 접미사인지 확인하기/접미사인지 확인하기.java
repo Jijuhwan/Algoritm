da9da5a3320d715cb5b9a7c63868dab3920c5155
Jijuhwan <38065739+Jijuhwan@users.noreply.github.com>
2024-03-11 07:47:25 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        int answer = 0;
+        if(is_suffix.length() > my_string.length()) return 0;
+  
+        if(my_string.substring(my_string.length() - is_suffix.length())
+           .equals(is_suffix)) answer = 1;
+        return answer;
+    }
+}
